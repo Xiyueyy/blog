522508bd2da54c4afb57c56cedd4f97907b361d3
Ruawd <Ruawd@noreply.gitcode.com>
2026-03-31 13:31:20 +0800

    fix: restore custom avatar from origin

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -69,8 +69,8 @@ export const siteConfig: SiteConfig = {
 		// 3. 本地图片（src目录，自动优化但会增加构建时间，推荐）: { type: "image", value: "assets/images/logo.webp", alt: "Logo" }
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
-			type: "url",
-			value: "https://blog.xiyy.de/_astro/avatar.m6eNDkHi_Z1JD341.webp",
+			type: "image",
+			value: "assets/images/avatar.jpg",
 			alt: "🍓",
 		},
 		// 导航栏标题
